893570fae0e9fd1fd1abb425e8b45ae960514709
Nicola Rovegno <nicolarovegno1@gmail.com>
2023-10-25 19:30:39 -0500

    test

--- main.py
+++ main.py
@@ -1,95 +1,87 @@
 # Example file showing a circle moving on screen
-import pygame
+import pygame as pg
 import abilities
 import constant
+import random
+from settings import *
+from sprites import *
+
 # pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-time = 0
-jumps = 1
-jump_time = 0
-jumping = False
-dy = 0
-accel = 14
-downward_accel = 1
-grounded = 1
-gravity = constant.GRAVITY
-direc_horizontal = -1  # -1 for left, 1 for right
-x = abilities.Dash()
-x.func()
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class Game:
+    def __init__(self):
+        # initialize game window, etc
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    def new(self):
+        # start a new game
+        dt = self.clock.tick(60) / 1000
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        x = abilities.Dash()
+        x.func()
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        self.run()
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    if x.active:
-        time += dt
-        if time > 0.3:
-            time = 0
-            x.active = False
-        else:
-            counter = 0
-            while counter < 4:
-                player_pos.x += 200 * dt * direc_horizontal
-                counter += 1
-    else:
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] and jumps > 0:
-            gravity = 0
-            jumps -= 1
-            jumping = True
-            dy = -60
-        if jumping:
-            counter = 0
-            jump_time += dt
-            accel -= 0.3
-            if jump_time > 0.7:
-                accel = 14
-                jump_time = 0
-                gravity = constant.GRAVITY
-                jumping = False
-                dy = 0
-        if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
-        if keys[pygame.K_a]:
-            direc_horizontal = -1
-            player_pos.x -= 300 * dt
-        if keys[pygame.K_d]:
-            direc_horizontal = 1
-            player_pos.x += 300 * dt
-        if keys[pygame.K_1]:
-            if x.current == 0:
-                x.current = x.cooldown
-                x.active = True
-    if x.current > 0:
-        x.current -= dt
-        if x.current < 0:
-            x.current = 0
-    # flip() the display to put your work on screen
-    player_pos.y += gravity * dt + dy * dt * accel
-    pygame.display.flip()
-    if player_pos.y < 600:
-        gravity = constant.GRAVITY * downward_accel
-        downward_accel += 0.1
-    else:
-        gravity = 0
-        downward_accel = 1
-        player_pos.y = 600
-        grounded = True
-        jumps = 1
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
 
-pygame.quit()
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+        # check if player hits a platform - only if falling
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over/continue
+        pass
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,53 @@
+import pygame as pg
+from settings import *
+vec = pg.math.Vector2
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game):
+        pg.sprite.Sprite.__init__(self)
+        self.game = game
+        self.image = pg.Surface((40, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.jumps = 7
+
+    def update(self):
+        self.acc = vec(0, 0.1)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+        if keys[pg.K_w] and self.jumps > 0:
+            self.jump()
+        # apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        # wrap around the sides of the screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+    def jump(self):
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        if hits:
+            self.vel.y = -7
+            self.jumps -= 1
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
